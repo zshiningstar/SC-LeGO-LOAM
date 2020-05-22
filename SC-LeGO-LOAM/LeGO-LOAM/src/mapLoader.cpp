edb796cd3b6c75a6085706417a09ccaff2c09153
AdamShan <zebang.shen@daimler.com>
2020-05-22 18:11:56 +0800

    modified the param for our vehicle

---
+++ SC-LeGO-LOAM/LeGO-LOAM/src/mapLoader.cpp
@@ -0,0 +1,14 @@
+
+#include "Scancontext.h"
+#include "utility.h"
+
+
+
+int main(int argc, char** argv)
+{
+    ros::init(argc, argv, "map_loader");
+
+    ROS_INFO("\033[1;32m---->\033[0m Map Loader Started.");
+
+    return 0;
+}
